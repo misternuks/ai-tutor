4a69fc465fdba8dcf83495a445ddff9d5f8162b2
Matthew Miner <misternuks@gmail.com>
2023-11-15 19:36:58 +0900

    modify controllers for message broadcast; refactor message view into a partial

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,6 +7,12 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data)
+
+    }
+    )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
