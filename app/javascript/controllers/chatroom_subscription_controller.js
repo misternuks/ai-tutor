c35fa21b10549ee9c7c0aa252adebb8c58b65f59
Matthew Miner <misternuks@gmail.com>
2023-11-15 20:07:19 +0900

    add scrolldown and form-clear feature to the chatroom

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -9,10 +9,18 @@ export default class extends Controller {
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data)
+      { received: data => this.#insertMessageAndScrollDown(data)
 
     }
     )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
   }
 }
