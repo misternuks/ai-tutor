03634ea4d65056c0766f681f087cb5975859c58c
Matthew Miner <misternuks@gmail.com>
2024-01-01 11:28:30 +0900

    adjust units controller methods and units new / edit views to allow creating, editing, and viewing of units per course

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -10,14 +10,16 @@ class UnitsController < ApplicationController
   end
 
   def new
+    @course = Course.find(params[:course_id])
     @unit = Unit.new
   end
 
   def create
+    @course = Course.find(params[:course_id])
     @unit = Unit.new(unit_params)
-
+    @unit.course = @course
     if @unit.save
-      redirect_to @unit
+      redirect_to course_path(@course)
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,13 +27,15 @@ class UnitsController < ApplicationController
 
   def edit
     @unit = Unit.find(params[:id])
+    @course = @unit.course
   end
 
   def update
     @unit = Unit.find(params[:id])
+    @course = @unit.course
 
     if @unit.update(unit_params)
-      redirect_to @unit
+      redirect_to course_path(@course)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -39,13 +43,13 @@ class UnitsController < ApplicationController
 
   def destroy
     @unit = Unit.find(params[:id])
+    @course = @unit.course
     @unit.destroy
-
-    redirect_to root_path, status: :see_other
+    redirect_to course_path(@course), status: :see_other
   end
 
   private
   def unit_params
-    params.require(:unit).permit(:name, :details)
+    params.require(:unit).permit(:name, :details, :course_id)
   end
 end
