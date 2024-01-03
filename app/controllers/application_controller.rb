406c6fd6f413e16cb950526aeb318f4e4fcd19c8
Matthew Miner <misternuks@gmail.com>
2024-01-03 16:07:24 +0900

    add username to sanitizer and username field in devise register view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protect_from_forgery
   skip_before_action :verify_authenticity_token, if: :json_request?
 
   protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 
   def json_request?
     request.format.json?
