7ed73b381abaf3b70eec69f01ecac8c18123e724
Matthew Miner <misternuks@gmail.com>
2024-06-01 19:57:26 +0900

    configure action mailer / devise settings for mailing e.g. password resets etc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,23 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Configure Action Mailer to use Gmail for sending emails (i.e. for Devise)
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: ENV['GMAIL_USERNAME']}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,19 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
   # Settings specified here will take precedence over those in config/application.rb.
+  # Action Mailer settings for Devise etc.
+  config.action_mailer.default_url_options = { host: "https://kuis-tutor-ai-a4262a31991a.herokuapp.com/" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
