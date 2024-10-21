08843b0dfc315328c61d1305d0cf5f3452bdda02
Matthew Miner <misternuks@gmail.com>
2024-10-21 11:48:05 +0900

    set redis verify_mode to SSL: verify_none for testing production issues with certification

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,5 @@
 redis = Redis.new(
   url: ENV["REDIS_URL"],
   ssl: true,
-  ssl_params: {
-    verify_mode: OpenSSL::SSL::VERIFY_PEER # Ensure this is set to verify the SSL cert properly
-  }
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
 )
